20db5e3f5028ec16f320cc4ebcd25aa15b2bfd56
rgw65206-sketch <rgw65206@gmail.com>
2025-12-05 12:16:43 +0900

    4강

--- src/main/java/com/back/domain/post/post/controller/ApiV1PostController.java
+++ src/main/java/com/back/domain/post/post/controller/ApiV1PostController.java
@@ -1,25 +1,34 @@
 package com.back.domain.post.post.controller;
 
+import com.back.domain.post.post.dto.PostDto;
 import com.back.domain.post.post.entity.Post;
 import com.back.domain.post.post.service.PostService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+@RestController
+@RequestMapping("/api/v1/posts")
 @RequiredArgsConstructor
 public class ApiV1PostController {
     private final PostService postService;
 
-    @GetMapping("/api/v1/posts")
-    @ResponseBody
-    public List<Post> getItems(){
+    @GetMapping
+    public List<PostDto> getItems() {
         List<Post> items = postService.findAll();
 
-        return items;
+        return items
+                .stream()
+                .map(post -> new PostDto(post))
+                .toList();
     }
-
+    @GetMapping("/{id}")
+    public PostDto getItem(@PathVariable int id){
+        Post post = postService.findById(id).get();
+        return new PostDto(post);
+    }
 }
